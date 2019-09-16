874ebf112c50923c46c80c74b8f589f5fa6c0a57
Morrison Cole <admin@morrisoncole.co.uk>
2019-09-16 15:01:40 +0900

    Solved rotate array problem in O(n) time and O(1) space

--- src/main/java/com/morrisoncole/solutions/leetcode/array/RotateArray.java
+++ src/main/java/com/morrisoncole/solutions/leetcode/array/RotateArray.java
@@ -3,6 +3,28 @@ package com.morrisoncole.solutions.leetcode.array;
 public class RotateArray {
 
     public void rotate(int[] nums, int k) {
+        k = k % nums.length;
 
+        if (k == 0) {
+            return;
+        }
+
+        int temp;
+        int correctPlacements = 0;
+
+        for (int originalPosition = 0; correctPlacements < nums.length; originalPosition++) {
+            int currentPosition = originalPosition;
+            int previousValue = nums[originalPosition];
+
+            do {
+                int nextPosition = (currentPosition + k) % nums.length;
+                temp = nums[nextPosition];
+                nums[nextPosition] = previousValue;
+                previousValue = temp;
+
+                currentPosition = nextPosition;
+                correctPlacements++;
+            } while (currentPosition != originalPosition);
+        }
     }
 }
--- src/test/java/com/morrisoncole/solutions/leetcode/array/RotateArrayTest.java
+++ src/test/java/com/morrisoncole/solutions/leetcode/array/RotateArrayTest.java
@@ -1,6 +1,5 @@
 package com.morrisoncole.solutions.leetcode.array;
 
-import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -10,14 +9,47 @@ import static com.google.common.truth.Truth.assertThat;
 
 class RotateArrayTest {
 
+    private RotateArray rotateArray = new RotateArray();
+
     @Test
-    @Ignore
-    void rotatesArray() {
-        RotateArray rotateArray = new RotateArray();
+    void rotatesEvenArray() {
+        int[] input = new int[]{1, 2};
+        List<Integer> expected = Arrays.asList(2, 1);
+
+        rotateArray.rotate(input, 1);
+
+        assertThat(input).asList()
+                .containsExactlyElementsIn(expected).inOrder();
+    }
+
+    @Test
+    void rotatesOddArray() {
         int[] input = new int[]{1, 2, 3, 4, 5, 6, 7};
         List<Integer> expected = Arrays.asList(5, 6, 7, 1, 2, 3, 4);
 
         rotateArray.rotate(input, 3);
+
+        assertThat(input).asList()
+                .containsExactlyElementsIn(expected).inOrder();
+    }
+
+    @Test
+    void doesNotRotateArrayWhenKIsZero() {
+        int[] input = new int[]{1, 2, 3, 4, 5, 6, 7};
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+
+        rotateArray.rotate(input, 0);
+
+        assertThat(input).asList()
+                .containsExactlyElementsIn(expected).inOrder();
+    }
+
+    @Test
+    void returnsIdentityWhenKIsEqualToLength() {
+        int[] input = new int[]{1, 2};
+        List<Integer> expected = Arrays.asList(1, 2);
+
+        rotateArray.rotate(input, 2);
 
         assertThat(input).asList()
                 .containsExactlyElementsIn(expected).inOrder();
