b80fbb9bb2d072d274460dd04038bedb419ff06a
Morrison Cole <admin@morrisoncole.co.uk>
2020-04-01 07:35:11 +0900

    chore: upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    kotlin("jvm") version "1.3.50"
+    kotlin("jvm") version "1.3.71"
 }
 
 version = "1.0-SNAPSHOT"
@@ -14,11 +14,11 @@ dependencies {
 
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit"))
-    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.5.2")
-    testImplementation("org.junit.jupiter", "junit-jupiter-params", "5.5.2")
-    testImplementation("com.google.truth", "truth", "1.0")
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.6.1")
+    testImplementation("org.junit.jupiter", "junit-jupiter-params", "5.6.1")
+    testImplementation("com.google.truth", "truth", "1.0.1")
 
-    testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", "5.5.2")
+    testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", "5.6.1")
 }
 
 configure<JavaPluginConvention> {
