4fc21481894149c5e9b4859af8a559521fa54fa0
Morrison Cole <admin@morrisoncole.co.uk>
2020-04-02 19:36:20 +0900

    chore: suppress unused paramaters

--- src/main/kotlin/com/morrisoncole/solutions/leetcode/MedianOfTwoSortedArrays.kt
+++ src/main/kotlin/com/morrisoncole/solutions/leetcode/MedianOfTwoSortedArrays.kt
@@ -3,7 +3,7 @@ package com.morrisoncole.solutions.leetcode
 /**
  * Time: O(log(m+n)), where m = a.length, n = b.length
  */
+@Suppress("UNUSED_PARAMETER")
 fun findMedianSortedArrays(a: IntArray, b: IntArray): Double {
-
     return 0.0
 }
