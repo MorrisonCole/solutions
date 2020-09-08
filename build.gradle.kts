e633e237858061d6df02c95eb910da375c6675b3
Morrison Cole <admin@morrisoncole.co.uk>
2020-09-09 08:44:42 +0900

    dependencies: upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
     java
-    kotlin("jvm") version "1.3.71"
+    kotlin("jvm") version "1.4.0"
+    id("com.github.ben-manes.versions") version "0.31.0"
 }
 
 version = "1.0-SNAPSHOT"
@@ -10,22 +13,33 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-
-    testImplementation(kotlin("test"))
-    testImplementation(kotlin("test-junit"))
-    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.6.1")
-    testImplementation("org.junit.jupiter", "junit-jupiter-params", "5.6.1")
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.6.2")
+    testImplementation("org.junit.jupiter", "junit-jupiter-params", "5.6.2")
     testImplementation("com.google.truth", "truth", "1.0.1")
 
-    testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", "5.6.1")
+    testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", "5.6.2")
 }
 
-configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+java {
+    sourceCompatibility = JavaVersion.VERSION_14
+    targetCompatibility = JavaVersion.VERSION_14
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+
+    named("dependencyUpdates", DependencyUpdatesTask::class.java).configure {
+        rejectVersionIf {
+            isNonStable(candidate.version) && !isNonStable(currentVersion)
+        }
+    }
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
 }
